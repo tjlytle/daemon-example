f24364797dbb906f5dc263f83e28d6f7a9c531ed
Tim Lytle <tim@timlytle.net>
2017-11-15 12:46:18 -0500

    🐳 Updated paths for docker.

--- wakeup/seed.php
+++ wakeup/seed.php
@@ -1,8 +1,8 @@
-#!/usr/bin/php
+#!/usr/local/bin/php
 <?php
 //autoloading and config
-require_once '../vendor/autoload.php';
-$config = include '../config.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+$config = include __DIR__ . '/../config.php';
 $service = new \Wakeup\Service($config);
 
 //setup queue
